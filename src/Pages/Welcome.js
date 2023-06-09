001cc46f1e1b31b0813394c9d68768983afea61a
ShirshakShahi <royalshirshal000@gmail.com>
2023-06-09 13:35:16 +0545

    added nested routes

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,6 +1,14 @@
+import { Routes, Route } from 'react-router-dom';;
 const Welcome = () => {
-    return <h3>
-        The Welcome Page
-    </h3>
+    return <>
+        <h3>
+            The Welcome Page
+        </h3>
+        <Routes>
+            <Route path="/welcome/new-user" element={
+                    <p>Hello new user!!!</p>
+            } />
+        </Routes>
+    </>
 }
 export default Welcome;
